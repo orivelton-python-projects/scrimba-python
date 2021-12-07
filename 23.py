b46f93179ead97e2f401331407307f2af48710be
Orivelton Cesar <orivelton10@gmail.com>
2021-12-07 18:53:20 +0000

    chore: Lesson 24

---
+++ 23.py
@@ -0,0 +1,9 @@
+# 23. Functions - Calling, parameters, arguments, defaults
+
+def greeting(name,age=28):
+    print("Hello " + name + ", you are " + str(age) + "!")
+    print(f"Hello {name}, you are {age}!")
+
+name = input("Enter your name: ")    
+greeting(name,32)
+greeting("Judith")
