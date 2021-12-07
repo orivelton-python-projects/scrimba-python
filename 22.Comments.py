6dcd955f45b251bf511e279a96c54a95e1c275b8
Orivelton Cesar <orivelton10@gmail.com>
2021-12-07 18:42:53 +0000

    chore: Lesson 22

---
+++ 22.Comments.py
@@ -0,0 +1,12 @@
+print("#Comments")
+#Hiding in the comments
+#fruits holds the fruits input by user as strings
+# fruits = ['apple', 'orange', 'banana', 'avocado']
+
+#Entry form for Ministry applications
+#to-do: fix it! it doesn’t work  
+name = "Default"
+name = input("Enter your silly name: ")
+print("Thank you " + name + "!")
+print("for applying to")
+print("the Minstry of Silly Walks")
